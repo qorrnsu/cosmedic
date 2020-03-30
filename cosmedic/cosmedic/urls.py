ac9f8c269553ddc8016340dbe2bdb88e1acc1f4f
qorrnsu@gmail.com <qorrnsu@gmail.com>
2020-03-30 03:48:14 -0700

    minor: remove slash after endpoint

--- cosmedic/cosmedic/urls.py
+++ cosmedic/cosmedic/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import handler
 
 urlpatterns = [
-    url(r'^search/$', handler.search),
-    url(r'^score/$', handler.get_score),
+    url(r'^search$', handler.search),
+    url(r'^score$', handler.get_score),
 
 ]
